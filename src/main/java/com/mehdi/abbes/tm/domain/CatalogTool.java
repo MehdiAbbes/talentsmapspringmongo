bc57835f1061123ac1b2291db79aa58d01acd8f6
Mehdi BEN HAJ ABBES <mehdi.abbes@gmail.com>
2013-01-03 18:02:27 +0100

    rest of interface with jersey

--- src/main/java/com/mehdi/abbes/tm/domain/CatalogTool.java
+++ src/main/java/com/mehdi/abbes/tm/domain/CatalogTool.java
@@ -8,86 +8,86 @@ import org.springframework.hateoas.Identifiable;
 @XmlRootElement
 @Persistent
 public class CatalogTool implements Identifiable<String> {
-
-	private String id;
-
-	private String toolLabel;
-
-	private String toolDescription;
-
-	private int score;
-
-	private boolean modified;
-
-	public String getToolLabel() {
-		return toolLabel;
-	}
-
-	public void setToolLabel(final String toolLabel) {
-		this.toolLabel = toolLabel;
-	}
-
-	public String getToolDescription() {
-		return toolDescription;
-	}
-
-	public void setToolDescription(final String toolDescription) {
-		this.toolDescription = toolDescription;
-	}
-
-	public int getScore() {
-		return score;
-	}
-
-	public void setScore(final int score) {
-		this.score = score;
-	}
-
-	public boolean isModified() {
-		return modified;
-	}
-
-	public void setModified(final boolean modified) {
-		this.modified = modified;
-	}
-
-	@Override
-	public String getId() {
-		return id;
-	}
-
-	public void setId(final String id) {
-		this.id = id;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = (prime * result) + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		CatalogTool other = (CatalogTool) obj;
-		if (id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!id.equals(other.id)) {
-			return false;
-		}
-		return true;
-	}
-
+    
+    private String id;
+    
+    private String toolLabel = "";
+    
+    private String toolDescription = "";
+    
+    private int score;
+    
+    private boolean modified;
+    
+    public String getToolLabel() {
+        return this.toolLabel;
+    }
+    
+    public void setToolLabel(final String toolLabel) {
+        this.toolLabel = toolLabel;
+    }
+    
+    public String getToolDescription() {
+        return this.toolDescription;
+    }
+    
+    public void setToolDescription(final String toolDescription) {
+        this.toolDescription = toolDescription;
+    }
+    
+    public int getScore() {
+        return this.score;
+    }
+    
+    public void setScore(final int score) {
+        this.score = score;
+    }
+    
+    public boolean isModified() {
+        return this.modified;
+    }
+    
+    public void setModified(final boolean modified) {
+        this.modified = modified;
+    }
+    
+    @Override
+    public String getId() {
+        return this.id;
+    }
+    
+    public void setId(final String id) {
+        this.id = id;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = (prime * result) + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+    
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        final CatalogTool other = (CatalogTool) obj;
+        if (this.id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!this.id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
 }
--- src/main/java/com/mehdi/abbes/tm/jersey/ToolResource.java
+++ src/main/java/com/mehdi/abbes/tm/jersey/ToolResource.java
@@ -1,9 +1,12 @@
 package com.mehdi.abbes.tm.jersey;
 
-import java.util.List;
-
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,19 +14,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.mehdi.abbes.tm.domain.CatalogTool;
+import com.mehdi.abbes.tm.domain.JerseyToolList;
 import com.mehdi.abbes.tm.repository.CatalogToolRepository;
 
 @Component
 @Path("/jerseyTools")
 public class ToolResource {
-
-	@Autowired
-	private CatalogToolRepository repo;
-
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public List<CatalogTool> getTools() {
-		List<CatalogTool> tools = repo.findAll();
-		return tools;
-	}
+    
+    @Autowired
+    private CatalogToolRepository repo;
+    
+    @GET
+    @Produces({
+        MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
+    })
+    public JerseyToolList getTools() {
+        final JerseyToolList toolList = new JerseyToolList();
+        toolList.setList(this.repo.findAll());
+        return toolList;
+    }
+    
+    @GET
+    @Produces({
+        MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
+    })
+    @Path("/{id}")
+    public CatalogTool getTool(@PathParam("id") final String toolId) {
+        return this.repo.findOne(toolId);
+    }
+    
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String createNewTool() {
+        final CatalogTool newTool = this.repo.save(new CatalogTool());
+        return newTool.getId();
+    }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({
+        MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
+    })
+    public CatalogTool saveTool(final CatalogTool newTool) {
+        return this.repo.save(newTool);
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public void deleteToom(@PathParam("id") final String toolId) {
+        this.repo.delete(toolId);
+    }
 }
