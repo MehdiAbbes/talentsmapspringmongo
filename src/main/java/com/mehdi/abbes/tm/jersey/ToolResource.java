617e9a9bfd6596063998225741ad3d3a3c0f1d5b
Mehdi BEN HAJ ABBES <mehdi.abbes@gmail.com>
2013-01-04 17:54:34 +0100

    handling Response

--- src/main/java/com/mehdi/abbes/tm/jersey/ToolResource.java
+++ src/main/java/com/mehdi/abbes/tm/jersey/ToolResource.java
@@ -8,7 +8,11 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,6 +24,9 @@ import com.mehdi.abbes.tm.repository.CatalogToolRepository;
 @Component
 @Path("/jerseyTools")
 public class ToolResource {
+    
+    @Context
+    private UriInfo uriInfo;
     
     @Autowired
     private CatalogToolRepository repo;
@@ -39,29 +46,54 @@ public class ToolResource {
         MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
     })
     @Path("/{id}")
-    public CatalogTool getTool(@PathParam("id") final String toolId) {
-        return this.repo.findOne(toolId);
+    public Response getTool(@PathParam("id") final String toolId) {
+        return Response.ok(this.repo.findOne(toolId)).build();
     }
     
     @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String createNewTool() {
+    public Response createNewTool() {
         final CatalogTool newTool = this.repo.save(new CatalogTool());
-        return newTool.getId();
+        return Response.created(UriBuilder.fromUri(this.uriInfo.getAbsolutePath()).path("/" + newTool.getId()).build())
+            .build();
     }
     
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes({
+        MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
+    })
     @Produces({
         MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
     })
-    public CatalogTool saveTool(final CatalogTool newTool) {
-        return this.repo.save(newTool);
+    public Response createTool(final CatalogTool newTool) {
+        final CatalogTool createdTool = this.repo.save(newTool);
+        return Response.created(UriBuilder.fromUri(this.uriInfo.getAbsolutePath()).path("/" + createdTool.getId())
+                                    .build()).build();
+        
+    }
+    
+    @PUT
+    @Consumes({
+        MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
+    })
+    @Produces({
+        MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML
+    })
+    @Path("/{id}")
+    public Response saveTool(@PathParam("id") final String toolId, final CatalogTool newTool) {
+        final CatalogTool oldTool = this.repo.findOne(toolId);
+        if (oldTool == null) {
+            throw new IllegalStateException("WTF the tool to update does not exist");
+        }
+        newTool.setId(toolId);
+        return Response.ok(this.repo.save(newTool)).build();
+        
     }
     
     @DELETE
     @Path("/{id}")
-    public void deleteToom(@PathParam("id") final String toolId) {
+    public Response deleteToom(@PathParam("id") final String toolId) {
         this.repo.delete(toolId);
+        return Response.ok().build();
     }
 }
